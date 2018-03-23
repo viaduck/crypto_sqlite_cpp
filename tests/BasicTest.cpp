452a89a441106a48c542b05f172a32690b2e54fc
Alexandros Roussos <thealexroussos@gmail.com>
2018-03-23 14:58:57 +0100

    Added license

--- tests/BasicTest.cpp
+++ tests/BasicTest.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017-2018 The ViaDuck Project
+ *
+ * This file is part of cryptoSQLitepp.
+ *
+ * cryptoSQLitepp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cryptoSQLitepp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with cryptoSQLitepp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cryptosqlite/encryption/PlaintextCrypt.h>
 #include "BasicTest.h"
 #include "cryptosqlite/cryptosqlite.h"
